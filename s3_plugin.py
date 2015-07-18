f7ac086645525d5e42d06ceee3bd72be005c6e85
JMSwag <johnymoswag@gmail.com>
2015-07-18 01:42:36 -0700

    Removed extra string formatter

--- s3_plugin.py
+++ s3_plugin.py
@@ -123,6 +123,6 @@ class ProgressPercentage(object):
             self._seen_so_far += bytes_amount
             percentage = (self._seen_so_far / self._size) * 100
             sys.stdout.write(
-                "\r%s  %s / %s  (%.2f%%)" % (self._seen_so_far,
-                                             self._size, percentage))
+                "\r%s / %s  (%.2f%%)" % (self._seen_so_far,
+                                         self._size, percentage))
             sys.stdout.flush()
