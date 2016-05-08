457745f2866187a5740adc4465fefb19884d1f47
JMSwag <johnymoswag@gmail.com>
2016-05-08 00:40:03 -0700

    Updated for PyUpater plugin system upgrade

--- s3_plugin/s3_uploader.py
+++ s3_plugin/s3_uploader.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2014 JohnyMoSwag <johnymoswag@gmail.com>
+# Copyright (c) 2016 Digital Sapphire
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -38,21 +38,21 @@ log = logging.getLogger(__name__)
 
 class S3Uploader(BaseUploader):
 
-    def __init__(self):
-        super(S3Uploader, self).__init__()
-
-    def init(self, **kwargs):
-        self.file_list = kwargs.get(u'files', [])
+    def init_config(self, config):
         access_key = os.environ.get(u'PYU_AWS_ID')
         if access_key is None:
             raise UploaderError('Missing PYU_AWS_ID')
         secret_key = os.environ.get(u'PYU_AWS_SECRET')
         if secret_key is None:
             raise UploaderError(u'Missing PYU_AWS_SECRET')
+        # Try to get bucket from env var
         self.bucket_name = os.environ.get(u'PYU_AWS_BUCKET')
-        bucket_name = kwargs.get(u'object_bucket')
+        bucket_name = config.get(u'object_bucket')
+        # If there is a bucket name in the repo config we
+        # override the env var
         if bucket_name is not None:
             self.bucket_name = bucket_name
+        # If nothing is set throw an error
         if self.bucket_name is None:
             raise UploaderError(u'Bucket name is not set')
 
@@ -61,6 +61,10 @@ class S3Uploader(BaseUploader):
                           region_name='us-west-2')
 
         self.s3 = session.client('s3')
+
+    def set_config(self, config):
+        bucket_name = self.get_answer('Please enter a bucket name')
+        config['bucket_name'] = bucket_name
 
     def upload_file(self, filename):
         """Uploads a single file to S3
@@ -75,19 +79,16 @@ class S3Uploader(BaseUploader):
 
                 False - Upload Failed
         """
-        with ChDir(self.deploy_dir):
-            try:
-                self.s3.upload_file(filename, self.bucket_name, filename,
-                                    ExtraArgs={'ACL': 'public-read'},
-                                    Callback=ProgressPercentage(filename))
-                log.debug('Uploaded {}'.format(filename))
-                os.remove(filename)
-                return True
-            except Exception as e:
-                log.error('Failed to upload file')
-                log.debug(str(e), exc_info=True)
-                self.connect()
-                return False
+        try:
+            self.s3.upload_file(filename, self.bucket_name, filename,
+                                ExtraArgs={'ACL': 'public-read'},
+                                Callback=ProgressPercentage(filename))
+            log.debug('Uploaded {}'.format(filename))
+            return True
+        except Exception as e:
+            log.error('Failed to upload file')
+            log.debug(str(e), exc_info=True)
+            return False
 
 
 class ProgressPercentage(object):
--- setup.py
+++ setup.py
@@ -2,43 +2,24 @@ from setuptools import setup
 
 setup(
     name='PyUpdater-s3-Plugin',
-    version='2.5',
-
+    version='3.0',
     description='Amazon S3 plugin for PyUpdater',
-
     author='JohnyMoSwag',
     author_email='johnymoswag@gmail.com',
-
     url='https://github.com/JohnyMoSwag/PyUpdater-s3-Plugin',
-
     classifiers=['Development Status :: 5 - Production/Stable',
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 2.7',
                  'Intended Audience :: Developers',
                  'Environment :: Console',
                  ],
-
     platforms=['Any'],
-
-    scripts=[],
-
-    provides=['pyupdater.plugins',
-              ],
-
     install_requires=[
         'boto3',
-        'jms-utils >= 0.6.2',
+        'jms-utils >= 1.0.1',
         ],
-
-    py_modules=['s3_plugin'],
-
+    packages=['s3_plugin'],
     include_package_data=True,
-
-    entry_points={
-        'pyupdater.plugins.uploaders': [
-            's3 = s3_plugin:S3Uploader',
-        ],
-    },
-
+    namespace_packages = ['pyupdater.plugins'],
     zip_safe=False,
 )
